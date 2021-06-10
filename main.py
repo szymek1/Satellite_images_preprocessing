8a3b01a5ea8f07750439f96aa94deb7b7ebbd23c
Szymon Bogus <bogusszymon2000@gmail.com>
2021-06-11 01:12:10 +0200

    Add files via upload
    Another attemp to extract unuseful features. This time the metohod is based on application of different filters and the drawing a contours over unwanted elements. The tricky part is to somehow effectively subtract this image with contours from original image to keep only areas not inside contours

--- main.py
+++ main.py
@@ -1,55 +1,75 @@
 import numpy as np
-from sklearn.cluster import MeanShift, estimate_bandwidth
 import cv2
 
+image = cv2.imread('mean_shift_result.png')
+original_img = image.copy()
+image_copy2 = image.copy()
 
-img = cv2.imread('img15.png')
-img = cv2.medianBlur(img, 3)
+data_of_clusters = np.loadtxt("clusters.txt", dtype=str)
+data_of_clusters = data_of_clusters.astype(np.float64)
+#print(data_of_clusters[0]) = [151 131 120] <-- first color I want to detect
+cv2.imshow("original",original_img)
 
+blurred_img = cv2.GaussianBlur(image, (27, 27), 0)
+#hsv = cv2.cvtColor(blurred_img, cv2.COLOR_BGR2HSV)
 
-flat_image = img.reshape((-1,3)) #flattening the imgage so it is no longer 3 dimensional
-flat_image = np.float32(flat_image)
+threshold = np.array([70,30,20])#threshold added to Upper boundary
+#Lower = data_of_clusters[0]
+#Upper = data_of_clusters[0] #+ threshold
 
+#NEW SOLUTION!!!: each mask given for each data_of_clusters[i] by using cv2.bitwise_and
+#should be merged with each other and create one final mask of all clusters
+#the try threshold/subtraction etc...
+for i in range(21):
+    Lower = data_of_clusters[i]
+    Upper = data_of_clusters[i] + threshold
+    mask = cv2.inRange(blurred_img, Lower, Upper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
+    cnts = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    area = 0
+    for c in cnts:
+        area += cv2.contourArea(c)
+        cv2.drawContours(original_img, [c], 0, (0, 0, 0), 2)
 
-bandwidth = estimate_bandwidth(flat_image, quantile=0.06, n_samples=3000)
-ms = MeanShift(bandwidth, max_iter=800, bin_seeding=True)
-ms.fit(flat_image)
-labeled=ms.labels_ #labels each individual class into segements
-segments = np.unique(labeled)
-cluster_centers = ms.cluster_centers_
-cluster_centers = np.uint8(cluster_centers)
+    print(area)
+    #cv2.imshow('mask'+str(i), mask)
 
-textfile = open("clusters.txt", "w+")#here clusters centers will be saved
+    #cv2.imshow('original'+str(i), original_img)
 
-print('Number of segments: ', segments.shape[0])
+cv2.imshow("image_after_mask",cv2.absdiff(image_copy2,original_img))
+#subtraction_product = cv2.subtract(image_copy2,original_img)
+#final = cv2.bitwise_not(original_img,subtraction_product)
+#cv2.imshow("result",final)
+#inal_result = cv2.subtract(original_img,image_copy2)
+#v2.imshow("result of subtraction",final_result)
 
+#creating a mask
+#mask = cv2.inRange(blurred_img, Lower, Upper)
+#mask = cv2.erode(mask, None, iterations=2)
+#mask = cv2.dilate(mask, None, iterations=2)
+#cv2.imshow("mask",mask)
+#image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+#lower = np.array(data_of_clusters[0], dtype="uint8")
+#upper = np.array(data_of_clusters[0], dtype="uint8")
+#mask = cv2.inRange(image, lower, upper)
+#
+#kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+#opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
+#
+#cnts = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+#
+#area = 0
+#for c in cnts:
+#    area += cv2.contourArea(c)
+#    cv2.drawContours(original_img,[c], 0, (0,0,0), 2)
 
-total = np.zeros((segments.shape[0], 3), dtype=float)
-count = np.zeros(total.shape, dtype=float)
-for i, label in enumerate(labeled):
-    total[label] = total[label] + flat_image[i]
-    count[label] += 1
-avg = total/count
-avg = np.uint8(avg)
-
-#turning cluster_centers into list of tuples- [(),().....]
-#color_boundaries = []
-#for i in range(cluster_centers.shape[0]):
-#    color_boundaries.append((cluster_centers[i][0],cluster_centers[i][1],cluster_centers[i][2]))
-
-#saving cluster centers into the file
-np.savetxt("clusters.txt", cluster_centers)
-textfile.close()
-
-# cast the labeled image into the corresponding average color
-res = avg[labeled]
-result = res.reshape((img.shape))
-
-
-# show the result
-cv2.imshow('result',result)
-cv2.imwrite("mean_shift_result.png", result)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
+#print(area)
+#cv2.imshow('mask', mask)
+#cv2.imshow('original', original_img)
+#cv2.imshow('opening', opening)
+cv2.waitKey()
